5b4fa6a810b96a51cccdd7a29f9632f2973517a1
Drago <dragodominguezfb@gmail.com>
2022-10-10 19:48:57 -0500

    Clase dia 10 de octubre

---
+++ DispositivosMoviles/Data/DatabaseQueryDrago.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using DispositivosMoviles.Models;
+using SQLite;
+
+namespace DispositivosMoviles.Data
+{
+    public class DatabaseQueryDrago
+    {
+        readonly SQLiteAsyncConnection _database;
+        
+        public DatabaseQueryDrago(string dbPath)
+        {
+            _database = new SQLiteAsyncConnection(dbPath);
+
+            _database.CreateTableAsync<UserDrago>().Wait();
+            //_database.CreateTableAsync<TodoModelDrago>().Wait();
+
+            
+        }
+        #region CRUD
+
+        //Método insertar o actualizar
+
+        public Task<int> SaveUserAsync(UserDrago user)
+        {
+            if(user.Id != 0) 
+            { 
+                return _database.UpdateAsync(user); } 
+            else
+            {
+                return _database.InsertAsync(user);
+            }
+        }
+        #endregion
+    }
+}
---
+++ DispositivosMoviles/Models/UserDrago.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace DispositivosMoviles.Models
+{
+    public class UserDrago
+    {
+        [PrimaryKey, AutoIncrement]
+
+        public int Id { get; set; }
+
+        public string Usuario { get; set; }
+
+        public string Password { get; set; }
+
+        public string Email { get; set; }
+
+        public string ApPaterno { get; set; }
+
+        public string ApMaterno { get; set; }
+
+        public string Edad { get; set; }
+
+        public string Pais { get; set; }
+
+        public DateTime Fecha_De_Creacion { get; set; }
+    }
+}
